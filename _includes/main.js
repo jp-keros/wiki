eec922af74eb3d97e074ffb5a3f524ff8895fc0d
Keros <76528736+jp-keros@users.noreply.github.com>
2022-04-21 22:54:33 +0200

    Update main.js

--- _includes/main.js
+++ _includes/main.js
@@ -13,13 +13,3 @@ var baseURL = window.location.hostname.indexOf('github.') !== -1 ? '//rawcdn.git
 document.write('<link rel="stylesheet" href="' + baseURL + 'css/cookies-eu-banner.default.css"/>');
 document.write('<script src="' + baseURL + 'dist/cookies-eu-banner.min.js"></script>');
 
-/* Google Analytics */
-// jshint ignore:start
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-ga('set', 'anonymizeIp', true);
-ga('create', 'UA-35470243-2', 'auto');
-ga('send', 'pageview');
